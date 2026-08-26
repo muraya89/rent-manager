c98f1a55898bb96a4c04a7dca3848911b83c5333
Muraya <susanmuraya89@gmail.com>
2026-08-26 16:43:57 +0300

    feat(app): Check existing prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,28 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-});
+// globalthis is a shared storage area for the running app. We use it to remember whether a Prisma client already exists
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = new PrismaClient({ adapter });
+// reuse the existing Prisma client if it exists, otherwise create a new one
+function createPrismaClient() {
+  const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  });
+
+  return new PrismaClient({ adapter });
+}
+
+// create a new Prisma client if one doesn't already exist, otherwise reuse the existing one
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+// During development, save the Prisma client in the global object so that it can be reused across module reloads caused by HMR (Hot Module Replacement).
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
